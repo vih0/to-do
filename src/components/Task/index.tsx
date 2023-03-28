b895293e987b5fb16b900e3db5ec7431f35de074
Vih0 <araujovitoris1.4@gmail.com>
2023-03-27 23:42:40 -0300

    adicionando estilos

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,26 +1,49 @@
 import { useState } from 'react'
 import AddIcon from '../../assets/addIcon.svg'
+import Clipborad from '../../assets/Clipboard.svg'
+import {ContainerTask} from "./styles";
 
+
+
+interface Task {
+    text: string
+    id: Date
+}
 export function TaskForm() {
     const [task, setTask] = useState('')
     const [newTask, setNewtask] = useState<string[]>([])
+    const [isEmpty, setIsEmpty]=useState(true)
     function handleAddNewTask(event: any) {
         event.preventDefault()
-        setNewtask((state) => [...state, task])
+        if (task !== '') {
+            setNewtask((state) => [...state, task])
+            setTask('')
+            setIsEmpty(false)
+        }
 
     }
     return (
-        <>
+        <ContainerTask>
             <form action="" onSubmit={handleAddNewTask}>
                 <input type="text" placeholder="Adicione uma nova tarefa"
-                    onChange={(state) => setTask(state.target.value)} />
+                    onChange={(state) => setTask(state.target.value)} value={task} />
                 <button type="submit">Criar<img src={AddIcon} alt="" /></button>
 
             </form>
-            <ul>
-                {newTask.map(task => <li key={task}>{task}</li>)}
+            {isEmpty?
+                <ContainerTask>
+                    <img src={Clipborad} alt=".."/>
+                    <strong>Você ainda não tem tarefas cadastradas</strong>
+                    <p>Crie tarefas e organize seus itens a fazer</p>
+                 </ContainerTask> :
+                <div>
+                    <ul>
+                        {newTask.map(task => <li key={task}>{task}</li>)}
 
-            </ul>
-        </>
+                    </ul>
+                </div>
+              }
+
+        </ContainerTask>
     )
 }
